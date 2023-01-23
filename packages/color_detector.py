e91b4f4dce73bb6c4331404426a86ffa434c1618
Sharyat <sharyatsb@gmail.com>
2023-01-22 23:46:01 -0500

    added comments and cleaned up the code

--- packages/color_detector.py
+++ packages/color_detector.py
@@ -1,28 +1,23 @@
 import rospy
 import numpy as np
 import cv2
-from sensor_msgs.msg import Image
 from sensor_msgs.msg import CompressedImage
-from cv_bridge import CvBridge, CvBridgeError
-from std_msgs.msg import String
 
-message = "Hi, this works"
-print(message)
-
-
+# Image call back function. It takes the CompressedImage message and returns a CV2 image
 def image_callback(msg):
-    # cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
-    # print(cv2_img)
+    # CVbridge does not support CompressedImage type data therefore, numpy is used.
+    # Alternatively, frombuffer can also be used.
     np_arr = np.fromstring(msg.data, np.uint8)
     image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-    # print("Received an image!")
     Color_detect(image_np)
-    # return image_np;
 
 def Color_detect(image_np):
+    # initilize max_count variable to 0
     max_count = 0
+    # Convert BGR image to HSV
     img_hsv = cv2.cvtColor(image_np, cv2.COLOR_BGR2HSV)
 
+    # Create a mask with the lower and upper HSV values of red color
     mask = cv2.inRange(img_hsv, (0, 160, 70),(10, 255, 255))
     count = np.sum(mask)
     if count > max_count:
@@ -31,11 +26,13 @@ def Color_detect(image_np):
         print("OOPS! I can no longer see red :(")
 
 
-
+# main function to initilize image_listener node that subscribes to the camera topic
 def main():
     rospy.init_node('image_listener')
+    # subscriber function, first argument is the topic, second argument is the topic message type
+    # and the last argument is the callback function
     rospy.Subscriber("/csc22919/camera_node/image/compressed", CompressedImage, image_callback)
-    # print("main function works")
+    # rospy.spin() makes sure that the node keeps running until the program is manually killed.
     rospy.spin()
 
 
